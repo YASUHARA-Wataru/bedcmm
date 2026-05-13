699f24050e3dd980daa6c79a61d8d0cdfdc342cd
unknown <yasuhara.wataru.personal.work@gmail.com>
2026-05-13 13:24:02 +0900

    add pypi publish test

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ ext_modules = cythonize([
 
 setup(
     name="bedcmm",
-    version="2.41",
     author="WATARU YASUHARA",
     description="公開特許ベースのパターン抽出・通信多重化アルゴリズム",
     long_description=open("README.md", encoding="utf-8").read(),
